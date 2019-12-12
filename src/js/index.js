25fe003e2790749bcb145c69ae7848903f65022c
marielyn52 <marielyn.otero@gmail.com>
2019-12-12 01:17:33 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,32 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["spade", "club", "heart", "diamond"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
 };
